60710e1241a3736719ae6fe24cbbf502fc41de19
Danny Tuppeny <danny@tuppeny.com>
2019-01-08 13:01:50 +0000

    Attempt to repro https://github.com/DanTup/dart-code-repro-1368
    I figured it might be our use of document.positionAt() which looks bocus (document may have been updated), however this doesn't repro.
    
    https://github.com/DanTup/dart-code-repro-1368

--- src/extension.ts
+++ src/extension.ts
@@ -11,19 +11,59 @@ export function activate(context: vscode.ExtensionContext) {
     // This line of code will only be executed once when your extension is activated
     console.log('Congratulations, your extension "slow-completion" is now active!');
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.sayHello', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    });
-
-    context.subscriptions.push(disposable);
+    context.subscriptions.push(vscode.languages.registerCompletionItemProvider(
+        { scheme: 'file', language: 'markdown' },
+        new SlowCompletionProvider(),
+        '.',
+    ));
 }
 
 // this method is called when your extension is deactivated
 export function deactivate() {
+}
+
+class SlowCompletionProvider implements vscode.CompletionItemProvider {
+    readonly dummyCompletion = "toString";
+    async provideCompletionItems(
+        document: vscode.TextDocument,
+        position: vscode.Position,
+        token: vscode.CancellationToken,
+        context: vscode.CompletionContext,
+    ): Promise<vscode.CompletionList> {
+
+        const completion = this.getCompletions(document.getText(), document.offsetAt(position));
+        if (!completion)
+            return;
+
+        // Wait 5 seconds to simulate slow server...
+        await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+
+        const item = new vscode.CompletionItem(completion.replacementText);
+        item.range = new vscode.Range(
+            document.positionAt(completion.replacementOffset),
+            document.positionAt(completion.replacementOffset + completion.replacementLength),
+        );
+
+        return new vscode.CompletionList([item]);
+    }
+
+    getCompletions(text: string, offset: number): { replacementText: string, replacementOffset: number, replacementLength: number } {
+        // Clip everything after the cursor off.
+        text = text.substr(0, offset);
+        // Find the last dot.
+        const dotIndex = text.lastIndexOf('.');
+        // Find the part of the string that's been typed since.
+        const partial = text.substr(dotIndex + 1);
+        // Bail if this doesn't match our dummy completion.
+        if (!this.dummyCompletion.startsWith(partial))
+            return;
+
+        // Otherwise, return a completion with offsets that will replace the partial
+        // text with the full dummy replacement.
+        return {
+            replacementText: this.dummyCompletion,
+            replacementOffset: dotIndex + 1,
+            replacementLength: offset - dotIndex - 1,
+        }
+    }
 }
